edb96e3a3d14f653944b21d859208c9362e1e196
ortur <orturb@yandex.ru>
2025-02-09 21:03:43 +0300

    temp

--- lua/tablescheme/init.lua
+++ lua/tablescheme/init.lua
@@ -1,5 +1,12 @@
+local tablescheme = {}
+
 local function print_hello()
     print("Hello, world!")
 end
 
-vim.api.nvim_create_user_command("TableScheme", print_hello(), {})
+
+function tablescheme.setup()
+    vim.api.nvim_create_user_command("TableScheme", print_hello(), {})
+end
+
+return tablescheme
