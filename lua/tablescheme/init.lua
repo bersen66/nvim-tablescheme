16eaa2688a386f13ca4ede2b64ee4f8dc470e73e
ortur <orturb@yandex.ru>
2025-02-12 18:33:20 +0300

    Fix text

--- lua/tablescheme/init.lua
+++ lua/tablescheme/init.lua
@@ -1,10 +1,13 @@
 local tablescheme = {}
 
+
 local function show_valentine()
     vim.api.nvim_command('vnew')
 
     local buf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_win_set_buf(0, buf)
+    local win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_buf(win, buf)
+
     local lines = {
         "Первая строка текста",
         "Вторая строка текста",
@@ -16,7 +19,6 @@ local function show_valentine()
     vim.api.nvim_command('setlocal readonly')
     vim.api.nvim_command('wincmd x')
 end
-
 
 function tablescheme.setup(opts)
     vim.api.nvim_create_user_command("TableScheme", show_valentine, {})
