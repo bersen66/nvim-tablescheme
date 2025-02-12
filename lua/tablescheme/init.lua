b8ba1b4f2b233433894d6ba07f2bf6768f0ffd35
ortur <orturb@yandex.ru>
2025-02-12 18:30:20 +0300

    Add text

--- lua/tablescheme/init.lua
+++ lua/tablescheme/init.lua
@@ -2,6 +2,17 @@ local tablescheme = {}
 
 local function show_valentine()
     vim.api.nvim_command('vnew')
+
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_win_set_buf(0, buf)
+    local lines = {
+        "Первая строка текста",
+        "Вторая строка текста",
+        "Третья строка текста"
+    }
+
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+
     vim.api.nvim_command('setlocal readonly')
     vim.api.nvim_command('wincmd x')
 end
