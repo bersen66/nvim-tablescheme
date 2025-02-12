0e80f50efa4ad4eb2965a668f63f64e5e7473f2a
ortur <orturb@yandex.ru>
2025-02-12 18:40:13 +0300

    temp

--- lua/tablescheme/init.lua
+++ lua/tablescheme/init.lua
@@ -1,12 +1,10 @@
 local tablescheme = {}
 
-
 local function show_valentine()
     vim.api.nvim_command('vnew')
 
     local buf = vim.api.nvim_create_buf(false, true)
-    local win = vim.api.nvim_get_current_win()
-    vim.api.nvim_win_set_buf(win, buf)
+    vim.api.nvim_set_current_buf(buf)
 
     local lines = {
         "Первая строка текста",
